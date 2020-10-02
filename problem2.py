9eabffbbba8b60890097fd1dbf118386e4a6b2f7
ccallow <71406811+ccallow@users.noreply.github.com>
2020-10-02 12:35:01 -0700

    Update problem2.py

---
+++ problem2.py
@@ -0,0 +1,13 @@
+#Calculate the factorial of a number. 
+#A factorial is defined as:
+#5! = 1 * 2 * 3 * 4 * 5
+#5! = 120
+
+n=int(input("Enter a number: "))
+a=1
+
+for i in range(1, n+1):
+    a=a*i
+print(str(n) + "! = "+ str(a))
+
+
