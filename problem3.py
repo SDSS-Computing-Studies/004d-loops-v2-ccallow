302f584941fa9547eebd9ef67fbfce1a75a1ff74
ccallow <71406811+ccallow@users.noreply.github.com>
2020-10-02 12:39:50 -0700

    Update problem3.py

---
+++ problem3.py
@@ -0,0 +1,6 @@
+#Have the user enter an integer that is smaller than 10
+#Find the sum of the series:
+#1 + 11 + 111 + 1111 + ....
+#for the n terms, where the nth term has n number of 1's
+
+n=int(input("Enter a number smaller than 10: "))
