6c73aefb876a51c6f1ef527657ebf9c0eb00f651
ccallow <71406811+ccallow@users.noreply.github.com>
2020-10-02 12:54:11 -0700

    Update problem3.py

--- problem3.py
+++ problem3.py
@@ -4,3 +4,8 @@
 #for the n terms, where the nth term has n number of 1's
 
 n=int(input("Enter a number smaller than 10: "))
+a=1
+print("the sum of the series is ", end="")
+for i in range(1, n+1):
+    print(a, end="")
+    a=a+1
